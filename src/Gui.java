91e083d4abf6827a8c5fa0ba2567e847a86344d2
billybobjoe778 <adichattopadhyay@gmail.com>
2018-09-19 17:38:17 -0400

    Started GUI

--- src/Gui.java
+++ src/Gui.java
@@ -3,17 +3,30 @@ import java.awt.*;
 import java.awt.event.*;
 
 class Gui extends JFrame {
+	static int mode = 0;
+	static int ENCRYPTION = 1;
+	static int DECRYPTION = 2;
+	static boolean fileCheck = false;
+	static boolean done = false;
 
-	private JButton hello = new JButton("Hello");
+	private JButton decrypt = new JButton("Decrypt");
 	private JButton encrypt = new JButton("Encrypt");
-	
+	private JButton decryptButton = new JButton("Decrypt Text");
+	private JButton encryptButton = new JButton("Encrypt Text");
+	private JButton yes = new JButton("Yes");
+	private JButton no = new JButton("No");
+
 	private JTextField txtA = new JTextField();
 	private JTextField txtB = new JTextField();
-	// private JTextField txtC = new JTextField();
+	private JTextField prompt = new JTextField();
+	private JTextField txtC = new JTextField();
+	private JTextField error = new JTextField();
+	private JTextField txtD = new JTextField();
 
 	private JLabel lblA = new JLabel("Text :");
 	private JLabel lblB = new JLabel("Password :");
-	// private JLabel lblC = new JLabel("C :");
+	private JLabel lblC = new JLabel("Input Path :");
+	private JLabel lblD = new JLabel("Output Path :");
 
 	public Gui() {
 		setTitle("Encryption Service");
@@ -27,27 +40,41 @@ class Gui extends JFrame {
 	}
 
 	private void initComponent() {
-		encrypt.setBounds(300, 130, 80, 25);
-		hello.setBounds(300, 100, 80, 25);
+		encrypt.setBounds(350, 130, 200, 100);
+		decrypt.setBounds(350, 240, 200, 100);
+		encryptButton.setBounds(300, 130, 300, 200);
+		decryptButton.setBounds(300, 130, 300, 200);
+		yes.setBounds(350, 130, 200, 100);
+		no.setBounds(350, 240, 200, 100);
 
 		txtA.setBounds(100, 10, 700, 20);
 		txtB.setBounds(100, 35, 700, 20);
-		// txtC.setBounds(100,65,100,20);
+		prompt.setBounds(330, 90, 240, 30);
+		txtC.setBounds(100, 10, 700, 20);
+		txtD.setBounds(100, 65, 100, 20);
+		error.setBounds(300, 350, 300, 30);
 
 		lblA.setBounds(20, 10, 100, 20);
 		lblB.setBounds(20, 35, 100, 20);
-		// lblC.setBounds(20,65,100,20);
+		lblD.setBounds(20, 65, 100, 20);
+		lblC.setBounds(20, 10, 100, 20);
 
 		add(encrypt);
-		add(hello);
+		add(decrypt);
 
-		add(lblA);
-		add(lblB);
-		// add(lblC);
-
-		add(txtA);
-		add(txtB);
-		// add(txtC);
+		add(prompt);
+		prompt.setText("Would you like to Encrypt or Decrypt?");
+		prompt.setEditable(false);
+		error.setEditable(false);
+		
+		txtA.setVisible(true);
+		txtB.setVisible(true);
+		txtC.setVisible(true);
+		txtD.setVisible(true);
+		lblA.setVisible(true);
+		lblB.setVisible(true);
+		lblC.setVisible(true);
+		lblD.setVisible(true);
 	}
 
 	private void initEvent() {
@@ -58,50 +85,142 @@ class Gui extends JFrame {
 			}
 		});
 
-		encrypt.addActionListener(new ActionListener() {
+		decryptButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				performEncryption(e);
 			}
 		});
 
-		hello.addActionListener(new ActionListener() {
+		encryptButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				btnTambahClick(e);
+				performDecryption(e);
+			}
+		});
+
+		encrypt.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				mode = ENCRYPTION;
+				askIfFile(e);
+			}
+		});
+
+		decrypt.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				mode = DECRYPTION;
+				askIfFile(e);
+			}
+		});
+		yes.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				fileCheck = true;
+				if (mode == 1) {
+					createEncryptButton(e);
+				} else if (mode == 2) {
+					createDecryptButton(e);
+				}
+			}
+		});
+		no.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				fileCheck = false;
+				if (mode == 1) {
+					createEncryptButton(e);
+				} else if (mode == 2) {
+					createDecryptButton(e);
+				}
 			}
 		});
 	}
 
+	private void createEncryptButton(ActionEvent evt) {
+		prompt.setVisible(false);
+		yes.setVisible(false);
+		no.setVisible(false);
+		if (fileCheck == false) {
+			add(txtA);
+			add(txtB);
+			add(lblA);
+			add(lblB);
+			add(error);
+		}
+		else {
+			add(txtC);
+			add(txtB);
+			add(txtD);
+			add(lblD);
+			add(lblB);
+			add(lblC);
+			add(error);
+		}
+		error.setText("Type in the inputs, then hit the encrypt button.");
+		add(encryptButton);
+	}
+
+	private void createDecryptButton(ActionEvent evt) {
+		prompt.setVisible(false);
+		yes.setVisible(false);
+		no.setVisible(false);
+		if (fileCheck == false) {
+			add(txtA);
+			add(txtB);
+			add(lblA);
+			add(lblB);
+			add(error);
+		}
+		else {
+			add(txtC);
+			add(txtB);
+			add(txtD);
+			add(lblD);
+			add(lblB);
+			add(lblC);
+			add(error);
+		}
+		error.setText("Type in the inputs, then hit the decrypt button.");
+		add(decryptButton);
+	}
+
+	private void askIfFile(ActionEvent evt) {
+		encrypt.setVisible(false);
+		decrypt.setVisible(false);
+		add(yes);
+		add(no);
+		if (mode == 1) {
+			prompt.setText("Would you like to encrypt a file");
+		} else if (mode == 2) {
+			prompt.setText("Would you like to decrypt a file?");
+		}
+	}
+
 	private void performEncryption(ActionEvent evt) {
-		String text = txtA.getText();
-		String password = txtB.getText();
-		try {
-			Encryption e = new Encryption();
-			String encryptedText = e.encrypt(text, password);
-			System.out.println(encryptedText);
-		}
-		catch(Exception e) {
-			System.out.println(e);
-		}
-	}
 
-	private void btnTambahClick(ActionEvent evt) {
-		Integer x, y, z;
-		try {
-			x = Integer.parseInt(txtA.getText());
-			y = Integer.parseInt(txtB.getText());
-			z = x + y;
-			// txtC.setText(z.toString());
+		while (done = false) {
+			if (fileCheck == false) {
+				String text = txtA.getText();
+				String password = txtB.getText();
+				if (text == "" || password == "") {
+					error.setText("Type in the inputs, then hit the encrypt button.");
+				} else {
+					try {
+						Encryption e = new Encryption();
+						String encryptedText = e.encrypt(text, password);
+						System.out.println(encryptedText);
+					} catch (Exception e) {
+						error.setText("Something went wrong, try again.");
+					}
+				}
+			} else if (fileCheck == true) {
+			}
 
-		} catch (Exception e) {
-			System.out.println(e);
-			JOptionPane.showMessageDialog(null, e.toString(), "Error", JOptionPane.ERROR_MESSAGE);
 		}
 	}
 
-	public static void main(String[] args) { //remove eventually
+	private void performDecryption(ActionEvent evt) {
+		
+		}
+
+	public static void main(String[] args) { // remove eventually
 		Gui g = new Gui();
 		g.setVisible(true);
 	}
 }
-
-
